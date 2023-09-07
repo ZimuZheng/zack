85466afb8c183270a01d50feff1b63b5fa4f8a57
ZimuZheng <zhengzimu2017@gwu.edu>
2023-09-07 11:45:19 +0200

    Init repo

---
+++ src/main/java/com/zimu/zack/ZackApplication.java
@@ -0,0 +1,13 @@
+package com.zimu.zack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zimu/zack/ZackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zimu.zack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
